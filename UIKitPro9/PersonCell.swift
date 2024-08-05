7245a9a7076e4e354a7d5e0c3955c8b1127ab7f3
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-08-05 20:24:55 +0330

    Part1: Designing UICollectionVeiw cells & UICollectionView data sources

---
+++ UIKitPro9/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  UIKitPro9
+//
+//  Created by Sothesom on 15/05/1403.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var name: UILabel!
+}
--- UIKitPro9/ViewController.swift
+++ UIKitPro9/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // create custom cell:
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Error")
+        }
+        return cell
+    }
 
 }
 
